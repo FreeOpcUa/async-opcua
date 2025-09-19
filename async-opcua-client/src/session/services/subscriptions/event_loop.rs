55987f2991ebb465193b4574b27ad2a5da06ae4a
Einar Omang <einar.omang@cognite.com>
2025-09-19 14:42:30 +0200

    Extract the subscription event loop
    Another slightly weird feature that I want for some stuff I'm working
    on. One thing I'm increasingly looking at is having ways to create
    "advanced" clients using the SDK without relying on the `Session`
    directly.
    
    A barrier to that is the subscription event loop. It's challenging to
    implement yourself, and we will likely iterate on it in the future as
    well, meaning that implementations are likely to get out of sync without
    noticing.
    
    This PR pulls most of the event loop out into a separate file using
    generics, so that users could define their own event loop, with other
    ways to make publish requests. This would also let us make small
    modifications to make the event loop more modular and configurable.
    
    As a side-benefit, it also splits the very large subscription event loop
    function up a bit, and makes the state a bit more explict, which is
    probably a good thing.

--- async-opcua-client/src/session/services/subscriptions/event_loop.rs
+++ async-opcua-client/src/session/services/subscriptions/event_loop.rs
@@ -1,11 +1,12 @@
 use std::{sync::Arc, time::Instant};
 
-use futures::{future::Either, stream::FuturesUnordered, Future, Stream, StreamExt};
+use futures::Stream;
 use opcua_types::StatusCode;
-use tracing::debug;
 
 use crate::{
-    session::{session_debug, session_error},
+    session::services::subscriptions::event_loop_state::{
+        SubscriptionCache, SubscriptionEventLoopState,
+    },
     Session,
 };
 
@@ -31,13 +32,6 @@ pub enum SubscriptionActivity {
 pub(crate) struct SubscriptionEventLoop {
     session: Arc<Session>,
     trigger_publish_recv: tokio::sync::watch::Receiver<Instant>,
-    last_external_trigger: Instant,
-    // This is true if the client has received BadTooManyPublishRequests
-    // and is waiting for a response before making further requests.
-    waiting_for_response: bool,
-    // This is true if the client has received a no_subscriptions response,
-    // and is waiting for a manual trigger or successful response before resuming publishing.
-    no_active_subscription: bool,
 }
 
 impl SubscriptionEventLoop {
@@ -53,154 +47,44 @@ impl SubscriptionEventLoop {
         session: Arc<Session>,
         trigger_publish_recv: tokio::sync::watch::Receiver<Instant>,
     ) -> Self {
-        let last_external_trigger = *trigger_publish_recv.borrow();
         Self {
-            last_external_trigger,
             trigger_publish_recv,
             session,
-            waiting_for_response: false,
-            no_active_subscription: false,
         }
     }
 
     /// Run the subscription event loop, returning a stream that produces
     /// [SubscriptionActivity] enums, reporting activity to the session event loop.
     pub(crate) fn run(self) -> impl Stream<Item = SubscriptionActivity> {
+        let session_ref = self.session.clone();
+
         futures::stream::unfold(
-            (self, FuturesUnordered::new()),
-            |(mut slf, mut futures)| async move {
-                // Store the next publish time, or None if there are no active subscriptions.
-                let mut next = slf.session.next_publish_time(false);
-                let mut recv: tokio::sync::watch::Receiver<Instant> =
-                    slf.trigger_publish_recv.clone();
-
-                let res = loop {
-                    // Future for the next periodic publish. We do not send publish requests
-                    // if there are no active subscriptions. In this case, simply return the
-                    // non-terminating future.
-                    let next_tick_fut = if let Some(next) = next {
-                        if slf.waiting_for_response && !futures.is_empty() {
-                            Either::Right(futures::future::pending::<()>())
-                        } else {
-                            Either::Left(tokio::time::sleep_until(next.into()))
-                        }
-                    } else {
-                        Either::Right(futures::future::pending::<()>())
-                    };
-
-                    // If FuturesUnordered is empty, it will immediately yield `None`. We don't
-                    // want that, so if it is empty we return the non-terminating future.
-                    let next_publish_fut = if futures.is_empty() {
-                        Either::Left(futures::future::pending())
-                    } else {
-                        Either::Right(futures.next())
-                    };
-
-                    tokio::select! {
-                        // Both internal ticks and external triggers result in publish requests.
-                        v = recv.wait_for(|i| i > &slf.last_external_trigger) => {
-                            if let Ok(v) = v {
-                                if !slf.waiting_for_response {
-                                    debug!("Sending publish due to external trigger");
-                                    // On an external trigger, we always publish.
-                                    futures.push(slf.static_publish());
-                                    next = slf.session.next_publish_time(true);
-                                    slf.last_external_trigger = *v;
-                                } else {
-                                    debug!("Skipping publish due BadTooManyPublishRequests");
-                                }
-                            }
-                            slf.no_active_subscription = false;
-                        }
-                        _ = next_tick_fut => {
-                            // Avoid publishing if there are too many inflight publish requests.
-                            if !slf.no_active_subscription && futures.len()
-                                < slf
-                                    .session
-                                    .publish_limits_watch_rx
-                                    .borrow()
-                                    .max_publish_requests
-                            {
-                                if !slf.waiting_for_response {
-                                    debug!("Sending publish due to internal tick");
-                                    futures.push(slf.static_publish());
-                                } else {
-                                    debug!("Skipping publish due BadTooManyPublishRequests");
-                                }
-                            }
-                            next = slf.session.next_publish_time(true);
-                        }
-                        res = next_publish_fut => {
-                            match res {
-                                Some(Ok(more_notifications)) => {
-                                    if more_notifications
-                                        || futures.len()
-                                            < slf
-                                                .session
-                                                .publish_limits_watch_rx
-                                                .borrow()
-                                                .min_publish_requests
-                                    {
-                                        if !slf.waiting_for_response {
-                                            debug!("Sending publish after receiving response");
-                                            futures.push(slf.static_publish());
-                                            // Set the last publish time to to avoid a buildup
-                                            // of publish requests if exhausting the queue takes
-                                            // more time than a single publishing interval.
-                                            slf.session.next_publish_time(true);
-                                        } else {
-                                            debug!("Skipping publish due BadTooManyPublishRequests");
-                                        }
-                                    }
-                                    slf.waiting_for_response = false;
-                                    slf.no_active_subscription = false;
-                                    break SubscriptionActivity::Publish
-                                }
-                                Some(Err(e)) => {
-                                    match e {
-                                        StatusCode::BadTimeout => {
-                                            session_debug!(slf.session, "Publish request timed out");
-                                        }
-                                        StatusCode::BadTooManyPublishRequests => {
-                                            session_debug!(
-                                                slf.session,
-                                                "Server returned BadTooManyPublishRequests, backing off",
-                                            );
-                                            slf.waiting_for_response = true;
-                                        }
-                                        StatusCode::BadSessionClosed
-                                        | StatusCode::BadSessionIdInvalid => {
-                                            // If this happens we will probably eventually fail keep-alive, defer to that.
-                                            session_error!(slf.session, "Publish response indicates session is dead");
-                                            break SubscriptionActivity::FatalFailure(e);
-                                        }
-                                        StatusCode::BadNoSubscription => {
-                                            session_debug!(
-                                                slf.session,
-                                                "Publish response indicates that there are no subscriptions"
-                                            );
-                                            slf.no_active_subscription = true;
-                                        },
-                                        _ => ()
-                                    }
-                                    break SubscriptionActivity::PublishFailed(e)
-                                }
-                                // Should be impossible.
-                                None => break SubscriptionActivity::PublishFailed(
-                                    StatusCode::BadInvalidState,
-                                )
-                            }
-                        }
-                    }
-                };
-
-                Some((res, (slf, futures)))
+            SubscriptionEventLoopState::new(
+                self.session.session_id(),
+                self.trigger_publish_recv,
+                self.session.publish_limits_watch_rx.clone(),
+                move || {
+                    let session = session_ref.clone();
+                    async move { session.publish().await }
+                },
+                SessionSubscriptionCache {
+                    inner: self.session.clone(),
+                },
+            ),
+            |mut state| async move {
+                let res = state.iter_loop().await;
+                Some((res, state))
             },
         )
     }
+}
 
-    fn static_publish(&self) -> impl Future<Output = Result<bool, StatusCode>> + 'static {
-        let inner_session = self.session.clone();
-        async move { inner_session.publish().await }
+struct SessionSubscriptionCache {
+    inner: Arc<Session>,
+}
+
+impl SubscriptionCache for SessionSubscriptionCache {
+    fn next_publish_time(&mut self, update: bool) -> Option<Instant> {
+        self.inner.next_publish_time(update)
     }
 }

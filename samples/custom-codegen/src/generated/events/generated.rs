64c3c7924da16e19ad73f85cbe30b3a2ada03870
Einar Omang <einar.omang@cognite.com>
2025-12-04 09:45:50 +0100

    Respect dependent types in events codegen
    This was done kinda lazily, with just a blanket "types" module that
    needed to contain everything needed. This instead uses the
    `types` argument to the main nodeset generation to infer type paths.

--- samples/custom-codegen/src/generated/events/generated.rs
+++ samples/custom-codegen/src/generated/events/generated.rs
@@ -5,11 +5,6 @@
 // OPCUA for Rust
 // SPDX-License-Identifier: MPL-2.0
 // Copyright (C) 2017-2024 Einar Omang
-#[allow(unused)]
-mod types {
-    pub use crate::generated::types::*;
-    pub use ::opcua::types::*;
-}
 #[derive(Debug, opcua::Event)]
 #[opcua(
     identifier = "i=1003",
@@ -18,8 +13,8 @@ mod types {
 pub struct PnAssetChangedEventType {
     pub base: opcua::nodes::BaseEventType,
     pub own_namespace_index: u16,
-    pub asset_change: types::PnAssetChangeEnumeration,
-    pub asset_type: types::PnAssetTypeEnumeration,
+    pub asset_change: crate::generated::types::PnAssetChangeEnumeration,
+    pub asset_type: crate::generated::types::PnAssetTypeEnumeration,
 }
 #[derive(Debug, opcua::Event)]
 #[opcua(
@@ -30,21 +25,21 @@ pub struct PnDiagnosisAlarmType {
     pub base: opcua::core_namespace::events::AlarmConditionType,
     pub own_namespace_index: u16,
     pub api: u32,
-    pub accumulative: types::PnChannelAccumulativeEnumeration,
+    pub accumulative: crate::generated::types::PnChannelAccumulativeEnumeration,
     pub channel_error_type: u16,
     pub channel_number: u16,
-    pub direction: types::PnChannelDirectionEnumeration,
+    pub direction: crate::generated::types::PnChannelDirectionEnumeration,
     pub ext_channel_add_value: u32,
     pub ext_channel_error_type: u16,
-    pub help_text: types::LocalizedText,
-    pub maintenance: types::PnChannelMaintenanceEnumeration,
-    pub manufacturer_data: types::ByteString,
+    pub help_text: opcua::types::LocalizedText,
+    pub maintenance: crate::generated::types::PnChannelMaintenanceEnumeration,
+    pub manufacturer_data: opcua::types::ByteString,
     pub qualified_channel_qualifier: u32,
     pub slot: u16,
-    pub specifier: types::PnChannelSpecifierEnumeration,
+    pub specifier: crate::generated::types::PnChannelSpecifierEnumeration,
     pub subslot: u16,
     #[opcua(rename = "Type")]
-    pub __type: types::PnChannelTypeEnumeration,
+    pub __type: crate::generated::types::PnChannelTypeEnumeration,
     pub user_structure_identifier: u16,
 }
 #[derive(Debug, opcua::Event)]
